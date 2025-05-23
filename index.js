b30cf0d891ac5d85dd8040c5f137d7eddbe7d510
Sowmitra Guha <sowmmochy@gmail.com>
2025-05-23 15:43:28 +0600

    find group by id is done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -31,6 +31,12 @@ async function run() {
 
     app.get('/groups', async(req, res) => {
         const result = await groupsCollection.find().toArray();
+        res.send(result);
+    })
+    app.get('/groups/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await groupsCollection.findOne(query);
         res.send(result);
     })
 
