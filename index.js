46256b8a8f3c7faa093376155e8331177ad3d815
Sowmitra Guha <sowmmochy@gmail.com>
2025-05-23 13:06:54 +0600

    mongodb connected successfully

--- index.js
+++ index.js
@@ -1,12 +1,42 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+
+// MongoDb Codes
+
+
+const uri = process.env.MongodbURI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 
 app.get('/', (req, res) => {
     res.send('coffee server is ready');
@@ -17,3 +47,4 @@ app.listen(port, () => {
 });
 
 // serversite github repo: https://github.com/Programming-Hero-Web-Course4/b11a10-server-side-sowmitraguho
+
